ce443cf00f7cf91aebc0b285bf0152cd773e9b17
Shivam Gupta <shivamgupta@tripgain.com>
2025-05-17 07:35:57 +0530

    Delete backend directory

--- backend/utils/sendResponse.js
+++
@@ -1,8 +0,0 @@
-const sendResponse = (res, status, message, success, data = null) => {
-    const response = { message: message, success: success };
-    if (data) {
-      response.data = data;
-    }
-    return res.status(status).send(response);
-  };
-  module.exports = { sendResponse };
